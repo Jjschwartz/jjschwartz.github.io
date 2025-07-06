15a4b5b1f8ba44c18eca9f9e2a15b919e37cceab
Buildbot <buildbot@imbue.com>
2025-07-06 14:25:42 -0700

    typo fix

--- db.py
+++ db.py
@@ -129,7 +129,7 @@ PROJECTS = [
         title="miniDRL",
         description=(
             "Minimal implementations of distributed, recurrent, deep reinforcement learning algorithms (PPO, R2D2). "
-            "Distributed RL, especially recurrent RL, gets pretty complex fast, so I wanted to write easy-to-follow stand-alone implementations of some distributed RL algorithms"
+            "Distributed RL, especially recurrent RL, gets pretty complex fast, this project contains some easy-to-follow stand-alone implementations of some distributed RL algorithms"
         ),
         url="https://github.com/Jjschwartz/miniDRL"
     ),
